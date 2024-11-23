8f1265458a2bef41aacb9741a289039147cecb22
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-23 11:46:44 -0300

    feat: add usecases factories

--- 03-api-solid/src/http/controllers/authenticate.ts
+++ 03-api-solid/src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
-import { AuthenticateUseCase } from "@/use-cases/authenticate";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { InvalidCredentialsError } from "@/use-cases/error/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 
 export async function authenticate(
   request: FastifyRequest,
@@ -16,9 +15,12 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
-    const prismaUserRepository = new PrismaUsersRepository();
-    const authenticateUseCase = new AuthenticateUseCase(prismaUserRepository);
-    await authenticateUseCase.execute({ email, password });
+    const authenticateUseCase = makeAuthenticateUseCase();
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
