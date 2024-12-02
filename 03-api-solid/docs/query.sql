145b12268826f2c79eac368aa4609b422cd25937
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-01 22:34:45 -0300

    feat: add auth middleware

--- 03-api-solid/docs/query.sql
+++ 03-api-solid/docs/query.sql
@@ -1,3 +1,7 @@
 -- CTRL + ALT + Q to execute a query on current connection (extension: mtxr.sqltools)
 SELECT * FROM users;
+---
+UPDATE users
+SET "role" = 'ADMIN'
+WHERE id ='3d71d7f7-7089-48ad-95d3-13e508ab26a7';
 --
